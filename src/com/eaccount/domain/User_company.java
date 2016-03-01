49867ce66f8d9ea1c90c5570940bdc457bef5736
yehao <492490295@qq.com>
2016-03-01 13:34:33 +0800

    添加getter

--- src/com/eaccount/domain/User_company.java
+++ src/com/eaccount/domain/User_company.java
@@ -7,4 +7,28 @@ public class User_company {
     private String id;
     private String user_id;
     private String company_id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(String company_id) {
+        this.company_id = company_id;
+    }
 }
