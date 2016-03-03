12046c417f3bf1d0361b7b39b34ab8704ee541a6
spzn <523113241@qq.com>
2016-03-03 13:21:08 +0800

    SPZn 2016/3/3 Add util-GetNowTime()

---
+++ src/com/eaccount/util/GetNowTime.java
@@ -0,0 +1,21 @@
+package com.eaccount.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by spzn on 16-3-3.
+ */
+public class GetNowTime {
+    public String GetTime(int type) {
+        DateFormat dateFormat = null;
+        if (type == 1) {
+            dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        } else if (type == 2) {
+            dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm/ss");
+        }
+        String date = dateFormat.format(new Date());
+        return date;
+    }
+}
