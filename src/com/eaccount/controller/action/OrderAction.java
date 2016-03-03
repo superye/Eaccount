ce2d53379c1a99e201f2b6cf51edb417f0ae7097
spzn <523113241@qq.com>
2016-03-03 20:54:17 +0800

    SPZn 2016/3/3 20:54

--- src/com/eaccount/controller/action/OrderAction.java
+++ src/com/eaccount/controller/action/OrderAction.java
@@ -2,6 +2,7 @@ package com.eaccount.controller.action;
 
 import com.eaccount.domain.Order;
 import com.opensymphony.xwork2.ModelDriven;
+import net.sf.json.JSONObject;
 
 /**
  * Created by spzn on 16-3-3.
@@ -10,7 +11,7 @@ public class OrderAction extends SuperAction implements ModelDriven<Order>{
     Order order = new Order();
 
     public String GetOrderMessage() {
-
+        JSONObject jsonObject = new JSONObject();
         return "GetOrderMessage";
     }
 
